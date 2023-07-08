e0e33dcd1ae1b87b596b09152a54da806d7438a6
anish979828 <girianish2004@gmail.com>
2023-07-08 20:26:53 +0530

    Completed Assignment level 1 in mongoose

--- block-BNaacj/mongoose.js
+++ block-BNaacj/mongoose.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose");
+
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
+var Schema = new Schema({
     name:{type: String,required:true},
     age:{type: Number, min:18, max:60},
     email:{type: String, lowercase:true, trim: true, match:/@/},
     password:{type:Number, minlength:5, maxlength:15},
     createdAt:{type: Date, default: new Date()}
-});
 
-module.exports = mongoose.model("User",userSchema);
+},{ timestamps: true });
--- block-BNaacm/model/article.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var articleSchema = new Schema({
-    name:{type: String,required:true},
-    description:{type:String,maxlength:20}
-});
-
-module.exports = mongoose.model("Article",articleSchema);
